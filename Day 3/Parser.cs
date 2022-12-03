aa2fe0f23319495b3850e1ea55fb7b9bfc2b3d98
Daylon Crider <daylon.crider@gmail.com>
2022-12-03 14:04:09 -0700

    Day 3 - Part 2

--- Day 3/Parser.cs
+++ Day 3/Parser.cs
@@ -4,7 +4,7 @@ class Parser
     private static readonly char[] lower = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     private static readonly char[] upper = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-    public static int Parse(string filePath)
+    public static int ParsePriorities(string filePath)
     {
         var sumOfPriorities = 0;
         using var reader = new StreamReader(filePath);
@@ -29,17 +29,55 @@ class Parser
 
             foreach(var letter in overlapping)
             {
-                if(lower.Contains(letter))
-                {
-                    sumOfPriorities += Array.IndexOf(lower,letter) + 1;
-                }
-                else if(upper.Contains(letter))
-                {
-                    sumOfPriorities += Array.IndexOf(upper, letter) + 27;
-                }
+                sumOfPriorities += GetLetterValue(letter);
             }
         }
 
         return sumOfPriorities;
+    }
+
+    public static int ParseBadges(string filePath)
+    {
+        var currentGroup = new List<string>();
+        var badgePrioritiesTotal = 0;
+        using var reader = new StreamReader(filePath);
+        while(!reader.EndOfStream)
+        {
+            while(currentGroup.Count() < 3)
+            {
+                currentGroup.Add(reader.ReadLine() ?? "");
+            }
+
+            var first = currentGroup[0];
+            var second = currentGroup[1];
+            var third = currentGroup[2];
+
+            var foundLetters = new List<char>();
+            foreach(var letter in first)
+            {
+                if(second.Contains(letter) && third.Contains(letter) && !foundLetters.Contains(letter))
+                {
+                    foundLetters.Add(letter);
+                    badgePrioritiesTotal += GetLetterValue(letter);
+                }
+            }
+            currentGroup.Clear();
+        }
+
+        return badgePrioritiesTotal;
+    }
+
+    private static int GetLetterValue(char letter)
+    {
+        if(lower.Contains(letter))
+        {
+            return Array.IndexOf(lower,letter) + 1;
+        }
+        else if(upper.Contains(letter))
+        {
+            return Array.IndexOf(upper, letter) + 27;
+        }
+
+        return 0;
     }
 }
--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -5,12 +5,14 @@ class Program
     {
         Console.Clear();
 
-        var sumOfPriorities = Parser.Parse("input.txt");
+        var sumOfPriorities = Parser.ParsePriorities("input.txt");
+        var badges = Parser.ParseBadges("input.txt");
 
         Console.WriteLine("Advent of Code 2022 - Day 3");
         Console.WriteLine("Solution by Daylon Crider");
         Console.WriteLine("https://adventofcode.com/2022/day/3");
         Console.WriteLine("===================================");
         Console.WriteLine("Sum of overlapping item priorities: " + sumOfPriorities);
+        Console.WriteLine("Sum of badge priorities: " + badges);
     }
 }
