cc289b9b60df060cad4a9ad290621ab82752a142
Daylon Crider <daylon.crider@gmail.com>
2022-12-04 16:05:55 -0700

    Day 4 - Part 2

--- Day 4/Parser.cs
+++ Day 4/Parser.cs
@@ -1,27 +1,59 @@
 namespace Day_4;
 class Parser
 {
-    public static int Parse(string filePath)
+    public static int ParseContains(string filePath)
+    {
+        var contains = 0;
+        var assignments = GetAllAssignments(filePath);
+        foreach(var pair in assignments)
+        {
+            var elf1 = pair[0];
+            var elf2 = pair[1];
+            if(elf1.IsContainedBy(elf2) || elf2.IsContainedBy(elf1))
+            {
+                contains++;
+            }
+        }
+        return contains;
+    }
+
+    public static int ParseOverlaps(string filePath)
     {
         var overlaps = 0;
-        using var reader = new StreamReader(filePath);
-        while(!reader.EndOfStream)
+        var assignments = GetAllAssignments(filePath);
+        foreach(var pair in assignments)
         {
-            var line = reader.ReadLine();
-            if(line == null) { break; }
+            var elf1 = pair[0];
+            var elf2 = pair[1];
 
-            var elf1 = line.Split(",")[0];
-            var elf2 = line.Split(",")[1];
-
-            var elf1Assignment = new CleaningAssignment(elf1);
-            var elf2Assignment = new CleaningAssignment(elf2);
-
-            if(elf1Assignment.IsContainedBy(elf2Assignment) || elf2Assignment.IsContainedBy(elf1Assignment))
+            if(elf1.Areas.Any(x => elf2.Areas.Contains(x)))
             {
                 overlaps++;
             }
         }
 
         return overlaps;
+    }
+
+    private static List<List<CleaningAssignment>> GetAllAssignments(string filePath)
+    {
+        var retval = new List<List<CleaningAssignment>>();
+        using var reader = new StreamReader(filePath);
+        while(!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            if(line == null){ break; }
+
+            var elf1 = line.Split(",")[0];
+            var elf2 = line.Split(",")[1];
+
+            var pair = new List<CleaningAssignment>();
+            pair.Add(new CleaningAssignment(elf1));
+            pair.Add(new CleaningAssignment(elf2));
+
+            retval.Add(pair);
+        }
+
+        return retval;
     }
 }
--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -5,12 +5,14 @@ class Program
     {
         Console.Clear();
 
-        var overlaps = Parser.Parse("input.txt");
+        var pairsContained = Parser.ParseContains("input.txt");
+        var overlaps = Parser.ParseOverlaps("input.txt");
 
         Console.WriteLine("Advent of Code 2022 - Day 3");
         Console.WriteLine("Solution by Daylon Crider");
         Console.WriteLine("https://adventofcode.com/2022/day/3");
         Console.WriteLine("===================================");
-        Console.WriteLine("Total cleaning assignment overlaps: " + overlaps);
+        Console.WriteLine("Total pairs with cleaning assignment contained within the other: " + pairsContained);
+        Console.WriteLine("Total pairs with cleaning assignment overlaps: " + overlaps);
     }
 }
